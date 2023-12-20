cfa1031133b738c421aeeeafdb1820e58a40c4b1
jwwals1 <joe_walsh59@yahoo.com>
2023-12-19 20:55:09 -0600

    added createContactPage eventListener

--- src/tabs.js
+++ src/tabs.js
@@ -1,24 +1,25 @@
 import createRestaurantHomePage from "./homepage";
 import createMenuPage from "./menu";
+import createContactPage from "./contact";
 
 const createTabs = () => {
     const content = document.querySelector('#content')
 
     const div1 = document.createElement('div');
     const div2 = document.createElement('div');
-    // const div3 = document.createElement('div');
+    const div3 = document.createElement('div');
 
     div1.setAttribute('id', 'home-button');
     div2.setAttribute('id', 'menu-button');
-    // div3.setAttribute('id', 'contact-btn');
+    div3.setAttribute('id', 'contact-btn');
 
     div1.textContent = 'Home';
     div2.textContent = 'Menu';
-    // div3.textContent = 'Content';
+    div3.textContent = 'Content';
 
     content.appendChild(div1);
     content.appendChild(div2);
-    // content.appendChild(div3);
+    content.appendChild(div3);
 
     div1.addEventListener('click', () => {
         clearContent();
@@ -30,9 +31,10 @@ const createTabs = () => {
         createMenuPage();
     })
 
-    //     div3.addEventListener('click', () => {
-    //         createRestaurantHomePage();
-    //     })
+    div3.addEventListener('click', () => {
+        clearContent();
+        createContactPage();
+    })
 }
 
 function clearContent() {
